267089de1868a17f425960e75a392679525f0eb7
EricYu2333 <yyqyu@ucdavis.edu>
2022-11-25 12:11:42 -0500

    update Auth

---
+++ Hotel-Booking-and-Reservation-System-EricYu V1/cmd/web/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"hotelManagement/internal/config"
+	"hotelManagement/internal/driver"
+	"hotelManagement/internal/handlers"
+	"hotelManagement/internal/helpers"
+	"hotelManagement/internal/models"
+	"hotelManagement/internal/render"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/alexedwards/scs/v2"
+
+	"time"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+var infoLog *log.Logger
+var errorLog *log.Logger
+
+func main() {
+	db, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.SQL.Close()
+
+	defer close(app.MailChan)
+
+	fmt.Println("Starting mail listener...")
+
+	listenForMail()
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() (*driver.DB, error) {
+	gob.Register(models.Reservation{})
+	gob.Register(models.User{})
+	gob.Register(models.Room{})
+	gob.Register(models.Restriction{})
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	// change this to true when in production
+	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	// connect to database
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=rose password=")
+	if err != nil {
+		log.Fatal("Cannot connect to database...")
+	}
+	log.Println("Connected to database")
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache ")
+		return nil, err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app, db)
+	handlers.NewHandlers(repo)
+
+	render.NewRenderer(&app)
+	helpers.NewHelpers(&app)
+
+	return db, nil
+}
---
+++ Hotel-Booking-and-Reservation-System-EricYu V1/go.mod
@@ -0,0 +1,13 @@
+module hotelManagement
+
+go 1.15
+
+require (
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
+	github.com/go-chi/chi v1.5.1
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/justinas/nosurf v1.1.1
+	github.com/xhit/go-simple-mail/v2 v2.13.0 // indirect
+)
