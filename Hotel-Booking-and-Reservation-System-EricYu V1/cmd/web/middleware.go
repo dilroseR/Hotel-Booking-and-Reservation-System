531cc4d146af6d514289190f1e21abe29098a3db
EricYu2333 <85422755+EricYu2333@users.noreply.github.com>
2022-11-25 12:36:44 -0500

    Add files via upload

---
+++ Hotel-Booking-and-Reservation-System-EricYu V1/cmd/web/middleware.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"hotelManagement/internal/helpers"
+	"net/http"
+
+	"github.com/justinas/nosurf"
+)
+
+// csrf middleware
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+// SessionLoad loads and saves session data for current request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first!")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+
+}
---
+++ Hotel-Booking-and-Reservation-System-EricYu V1/internal/config/config.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"hotelManagement/internal/models"
+	"html/template"
+	"log"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+// AppConfig holds the application config
+type AppConfig struct {
+	UseCache      bool
+	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
+	ErrorLog      *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
+	MailChan      chan models.MailData
+}
